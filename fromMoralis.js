f1797edab22581cc49938ef4cf232a05df6f0169
mearaf <mearaftadewos@gmail.com>
2023-05-16 18:00:27 +0300

    getting balace of arbitrary address

---
+++ fromMoralis.js
@@ -0,0 +1,14 @@
+
+const options = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json', 'X-API-Key': process.env.X},
+  };
+  fetch('https://deep-index.moralis.io/api/v2/block/1000?chain=eth', options)
+    .then((response) => response.json())
+    .then((response) => printResult(response))
+    .catch((err) => console.error(err))
+  
+  const printResult = (response) => {
+    const container = document.getElementById('result')
+    container.innerHTML = "<pre>" + JSON.stringify(response ,null, 2) + "</pre>"
+  }
---
+++ index.js
@@ -0,0 +1,60 @@
+const Moralis = require('moralis').default
+const express = require('express')
+const app = express()
+const port = 8080
+const cors = require("cors")
+require("dotenv").config()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/from_moralis', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/nativeBalance', async (req, res) => {
+
+  await Moralis.start({apiKey: process.env.MORALIS_API_KEY})
+  try {
+    const {address, chain} = req.query
+    const response = await Moralis.EvmApi.balance.getNativeBalance({
+      address: address,
+      chain: chain,
+    });
+    const nativeBalance = response.toJSON().balance
+    console.log("native Balance: ", nativeBalance)
+
+    let nativeCurrency   // wrapped contract
+    if (chain == "0x1") {
+      nativeCurrency = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2" 
+    } else if(chain == "0x89") {
+      nativeCurrency = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"
+    }
+    const nativePrice = await Moralis.EvmApi.token.getTokenPrice({
+      address: nativeCurrency,
+      chain: chain
+    })
+  
+    const totalInUsd = parseInt(nativeBalance) * nativePrice.toJSON().usdPrice
+    console.log("Native Price: ", nativePrice.toJSON().usdPrice)
+    console.log("Total in USD: ", totalInUsd)
+
+    nativeBalance['usdValue']= totalInUsd
+
+    const balance = {
+      balance: nativeBalance, 
+      balance_usd: totalInUsd
+    }
+    res.send(balance)
+  } catch (error) {
+    res.send(error)
+  }
+  // res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Xerxis Wallet is listening on port ${port}`)
+})
